fb00e8f506493742a8431c7140d2c5f4b6e4ff6a
Robin Phillips <rlphillips@deloitte.co.uk>
2024-04-22 13:01:20 +0100

    initial set up of package, project and workspace set up

---
+++ Homepage-DemoApp/Homepage-DemoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homepage-DemoApp
+//
+//  Created by Phillips, Robin on 22/04/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
