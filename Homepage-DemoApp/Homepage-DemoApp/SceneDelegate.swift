51cb55bcec951c116f4a390bca1c7b1ad91d082e
Robin Phillips <rlphillips@deloitte.co.uk>
2024-04-23 16:45:59 +0100

    swift lint fixes

--- Homepage-DemoApp/Homepage-DemoApp/SceneDelegate.swift
+++ Homepage-DemoApp/Homepage-DemoApp/SceneDelegate.swift
@@ -4,7 +4,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 
         guard let windowScene = (scene as? UIWindowScene) else { return }
@@ -44,6 +43,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
 
 }
