a4f410c813a3c8d388ce469c447e520736b0ff28
Robin Phillips <rlphillips@deloitte.co.uk>
2024-05-02 12:17:47 +0100

    swiftlint fixes

--- Homepage-DemoApp/Homepage-DemoApp/AppDelegate.swift
+++ Homepage-DemoApp/Homepage-DemoApp/AppDelegate.swift
@@ -2,7 +2,6 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -26,5 +25,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
 }
